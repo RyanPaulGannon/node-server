31b312576be9b6f7ef0c6821fe54facbf53afd2e
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-08-11 18:13:45 +0100

    Stripe work in more detail

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-import cors from 'cors'
-import express from 'express'
-import bodyParser from 'body-parser';
+require("dotenv").config()
+
+const cors = require("cors")
+const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express()
-const port = 5105
 
 app.use(bodyParser.json());
 app.use(
@@ -16,20 +17,43 @@ app.use(
     credentials: true,
 }))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 app.post('/checkout', (req, res) => {
-    console.log('Post successful')
     res.send(req.body)
     console.log(req.body)
 })
 
-app.get('/checkout', (req, res) => {
-  
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+
+const storeItems = new Map([
+  [1, { priceInCents: 1000, name: 'Test One'}],
+  [2, { priceInCents: 2000, name: 'Test Two'}]
+])
+
+app.post('/checkout-session', async (req, res) => {
+  try {
+    const session = stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: req.body.items.map(item => {
+        const storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        }
+      }),
+      success_url: `${process.env.CLIENT_URL}/success`,
+      cancel_url: `${process.env.CLIENT_URL}/cancel`,
+    })
+    res.json({ url: session.url })
+  } catch (e) {
+    res.status(500).json({ error: e.message })
+  }
 })
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+app.listen(5105)
