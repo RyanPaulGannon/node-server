a4ee12483dd58986cfc58b78d90c7fd5503b2693
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-12-20 08:08:05 -0500

    Header parser started

--- src/app.ts
+++ src/app.ts
@@ -15,12 +15,23 @@ app.get("/", (req: Request, res: Response) => {
   res.send("TS-Node Server")
 })
 
+/* Header Parser */
 app.get("/api/whoami", (req: Request, res: Response) => {
   res.json({
     ipaddress: req.socket.remoteAddress,
     language: req.headers["accept-language"],
     software: req.headers["user-agent"],
   })
+})
+
+/* URL Shortener */
+app.post("/api/shorturl", (req: Request, res: Response) => {
+  let urlRegex =
+    /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/
+
+  if (urlRegex.test(req.baseUrl)) return { error: "invalid url" }
+
+  res.json({ original_url: req.baseUrl, short_url: 1 })
 })
 
 /* listener */
