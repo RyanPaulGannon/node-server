9b7ac69a2ec5968d546dedb0b22bbb66efb22a1d
Ryan Gannon <ryanpaulgannon@icloud.com>
2022-12-23 21:40:34 +0000

    Added prisma for mongo connection

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,27 @@ const port = process.env.PORT || 4000
 app.use(cors({ origin: "*" }))
 app.use(express.json())
 
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export async function main() {
+  // Connect the client
+  await prisma.$connect()
+  console.log("Connected")
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
+
 /* routes */
 app.get("/", (req: Request, res: Response) => {
   res.send("TS-Node Server")
