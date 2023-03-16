261bbc2dc898e3c911fafa11d6db5e74b03a139f
Ryan Gannon <ryanpaulgannon@icloud.com>
2023-03-16 20:40:13 +0000

    Testing Prisma with local PostGres

--- src/database/connect.ts
+++ src/database/connect.ts
@@ -1,25 +1,3 @@
-import { CosmosClient } from '@azure/cosmos'
+import { prisma } from './client'
 
-const endpoint = process.env.AZURE_COSMOS_DATABASE_URL!
-const key = process.env.AZURE_PRIMARY_KEY_COSMOS!
-const client = new CosmosClient({ endpoint, key })
-
-const databaseName = 'YourWesthoughtonFood'
-const containerName = 'Restaurants'
-
-export async function connect() {
-  const database = client.database(databaseName)
-
-  const container = database.container(containerName)
-
-  await container.item('1').read()
-
-  const { resources } = await container.items
-    .query('SELECT * from c')
-    .fetchAll()
-  for (const restaurant of resources) {
-    console.log(`${restaurant.name} is a ${restaurant.cuisine} place`)
-  }
-
-  console.log(resources)
-}
+export async function connect() {}
